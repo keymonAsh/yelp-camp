3241e184e2b2d43344e55638b510cd21cc011606
Aswin Gangadharan <keymonash9@gmail.com>
2020-03-12 17:03:47 +0530

    landing and campground routes

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const express = require('express')
 const app = express();
 
+const campgrounds = [
+    {title: "Tile 1", img: ""},
+    {title: "Tile 2", img: ""},
+    {title: "Tile 3", img: ""}
+]
+
+app.set("view engine", "ejs")
+
 app.get("/", function(req, res) {
-    res.send("Yelp Camp")
+    res.render("landingpage")
+})
+
+app.get("/campgrounds",function(req, res) {
+    res.render("campgrounds", {campgrounds: campgrounds})
 })
 
 app.listen(3000, function() {
