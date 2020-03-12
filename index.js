0e38342db0c2fe6786dff95f1db9726af42fb853
Aswin Gangadharan <keymonash9@gmail.com>
2020-03-12 21:19:44 +0530

    add create route and form

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express();
+const bodyParser = require('body-parser')
 
-const campgrounds = [
-    {title: "Tile 1", img: ""},
-    {title: "Tile 2", img: ""},
-    {title: "Tile 3", img: ""}
+var campgrounds = [
+    {title: "Tile 1", img: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg"},
+    {title: "Tile 2", img: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg"},
+    {title: "Tile 3", img: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg"}
 ]
 
+app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
 
 app.get("/", function(req, res) {
@@ -15,6 +17,19 @@ app.get("/", function(req, res) {
 
 app.get("/campgrounds",function(req, res) {
     res.render("campgrounds", {campgrounds: campgrounds})
+})
+
+app.post("/campgrounds", function(req, res) {
+    let title = req.body.title
+    let image = req.body.image
+
+    campgrounds.push({title: title, img: image})
+
+    res.redirect("/campgrounds")
+})
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("new");
 })
 
 app.listen(3000, function() {
