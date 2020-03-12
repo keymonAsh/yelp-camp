12e79a934236c2707d4d553dd438bf30a3177e98
Aswin Gangadharan <keymonash9@gmail.com>
2020-03-12 22:53:06 +0530

    add database and newfrom connect

--- index.js
+++ index.js
@@ -1,35 +1,65 @@
 const express = require('express')
 const app = express();
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
-var campgrounds = [
-    {title: "Tile 1", img: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg"},
-    {title: "Tile 2", img: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg"},
-    {title: "Tile 3", img: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg"}
-]
+mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+
+// var campgrounds = [
+//     {title: "Tile 1", img: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg"},
+//     {title: "Tile 2", img: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg"},
+//     {title: "Tile 3", img: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg"}
+// ]
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.set("view engine", "ejs")
+
+// Schema
+
+const campgroundSchema = new mongoose.Schema({
+    title: String,
+    imgURL: String,
+    disc: String
+})
+
+const Campground = mongoose.model("Campground", campgroundSchema)
+
+// Rouuutes
 
 app.get("/", function(req, res) {
     res.render("landingpage")
 })
 
 app.get("/campgrounds",function(req, res) {
-    res.render("campgrounds", {campgrounds: campgrounds})
+    Campground.find({}, function(err, campgrounds) {
+        if(err) {
+            console.log(err)
+        } else {
+            res.render("campgrounds", {campgrounds: campgrounds})
+        }
+    })
 })
 
 app.post("/campgrounds", function(req, res) {
     let title = req.body.title
-    let image = req.body.image
+    let imgURL = req.body.imgURL
+    let newCampground = {title: title, imgURL: imgURL}
 
-    campgrounds.push({title: title, img: image})
-
-    res.redirect("/campgrounds")
+    Campground.create(newCampground, function(err) {
+        if(err) {
+            console,log(err)
+        } else {
+            res.redirect("campgrounds")
+        }
+    })
 })
 
 app.get("/campgrounds/new", function(req, res) {
     res.render("new");
+})
+
+app.get("/campgrounds/:id", function(req, res) {
+    
 })
 
 app.listen(3000, function() {
