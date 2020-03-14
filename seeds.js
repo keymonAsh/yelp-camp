6ee302d1fb7afc753f7bb1a9c586a6154673d7b1
Aswin Gangadharan <keymonash9@gmail.com>
2020-03-14 13:52:11 +0530

    minor styling

--- seeds.js
+++ seeds.js
@@ -3,9 +3,9 @@ const Campground = require('./models/campgrounds')
 const Comment = require('./models/comments')
 
 const data = [
-    {title: "moutain range", imgURL: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg", disc: "High AF"},
-    {title: "desert fun", imgURL: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg", disc: "Dry AF"},
-    {title: "Ocearic OLEE", imgURL: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg", disc: "WET AP"},
+    {title: "moutain range", imgURL: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg", disc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui, atque perferendis. Suscipit quas fugiat ipsum odit itaque, iusto quaerat incidunt. Consectetur modi rerum commodi, voluptate quibusdam quae a eos suscipit."},
+    {title: "desert fun", imgURL: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg", disc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem minus, mollitia temporibus vero ipsam reiciendis placeat dolore nesciunt obcaecati! Natus dolorem eius necessitatibus voluptatum sapiente officiis labore libero perspiciatis qui?"},
+    {title: "Ocearic OLEE", imgURL: "https://invinciblengo.org/photos/event/slider/manali-girls-special-adventure-camp-himachal-pradesh-1xJtgtx-1440x810.jpg", disc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis libero ipsum rem cum nam, asperiores debitis ex beatae possimus, iure ratione odit in dolorem numquam nesciunt voluptate, animi iste enim."},
 ]
 
 
@@ -16,6 +16,13 @@ function seedDb() {
             console.log(err)
         }
         console.log("Campgrounds Removed")
+    })
+
+    Comment.remove({}, function(err) {
+        if(err) {
+            console.log(err)
+        }
+        console.log("Comments Removed")
     })
 
     //add Campgrounds
